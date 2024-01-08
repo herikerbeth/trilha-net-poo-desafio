3bd4c889533b0de7370c43038786290193156ab5
herikerbeth <herik.docarmo@hotmail.com>
2024-01-08 10:44:39 -0300

    :test:Program:adicionado testes para as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizando testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("001", "modeloX1", "0101", 124);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("clash royale");
+
+Iphone iphone = new Iphone("002", "modeloY2", "0202", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ZapZap");
